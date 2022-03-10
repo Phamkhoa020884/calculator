544717d5640e1f64645735072645f60198a0c00d
khoa <hatgiong@gmail.com>
2022-03-11 05:37:59 +0700

    Initial commit

---
+++ lib/calculator_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatefulWidget {
+
+
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculator"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (text){
+
+              },
+              decoration: InputDecoration(
+                hintText: "Input text"
+              ),
+              keyboardType: TextInputType.number,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:calculator/calculator_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter widget",
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: CalculatorPage(),
+    );
+  }
+}
