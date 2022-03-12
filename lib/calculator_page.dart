114e8f29642d8b74895de171e9ad53993588a999
khoa <hatgiong@gmail.com>
2022-03-12 15:26:19 +0700

    iu

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -8,27 +8,106 @@ class CalculatorPage extends StatefulWidget {
 }
 
 class _CalculatorPageState extends State<CalculatorPage> {
+  final TextEditingController nuberOneController= TextEditingController();
+  double? result ;
+
   @override
   Widget build(BuildContext context) {
+    final double width =  MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Calculator"),
       ),
       body: Container(
+        constraints: BoxConstraints.expand(),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              onChanged: (text){
-
-              },
-              decoration: InputDecoration(
-                hintText: "Input text"
+            Text(
+              "Máy tính bỏ túi",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.red,
+                  fontSize: width/20,
+                  fontWeight: FontWeight.bold
               ),
-              keyboardType: TextInputType.number,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Input nuber 1",
+                  border: OutlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Input nuber 2",
+                    border: OutlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            showResult(),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text(" + ")
+                      ),
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text(" - ")
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text(" * ")
+                      ),
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text(" / ")
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
       ),
     );
+  }
+  Widget showResult(){
+    if (result != null){
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text("=10"),
+      );
+    }else{
+      return Container();
+    }
   }
 }
